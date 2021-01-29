5e6f7429e09bfedf839b2e36134516871f85db48
Nguyen Tran <75809911+remixcongaavn@users.noreply.github.com>
2021-01-28 18:57:40 -0600

    Assigment01

---
+++ Assets/Script/NameTransfer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class NameTransfer : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + theName + " to the game";
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Script/ShowValueScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValueScript : MonoBehaviour
+{
+    Text percentageText;
+
+    void Start ()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void textUpdate (float value)
+    {
+        percentageText.text = Mathf.RoundToInt(value * 100) + "";
+    }
+}
